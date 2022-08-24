4e7c83b2bfaf5e5674c262fd2d6e1f6106484f56
LR <erladoisl@gmail.com>
2022-08-24 14:21:25 +0300

    + model Price

--- fireapp/models.py
+++ fireapp/models.py
@@ -9,9 +9,8 @@ class Good(models.Model):
     class Meta:
         db_table = 'goods'
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
-
 
 
 class Link(models.Model):
@@ -22,5 +21,18 @@ class Link(models.Model):
     class Meta:
         db_table = 'links'
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
+
+
+class Price(models.Model):
+    date = models.DateField()
+    good_id = models.TextField()
+    link_id = models.TextField()
+    price = models.FloatField()
+
+    class Meta:
+        db_table = 'prices'
+
+    def __str__(self):
+        return self.price
